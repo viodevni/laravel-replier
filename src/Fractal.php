1deb424e17e31d57cbbc924b045cf25a23216d18
philvv <phillip@viodev.com>
2022-08-16 13:07:54 +0100

    Catch nulls in collections.

--- src/Fractal.php
+++ src/Fractal.php
@@ -30,7 +30,7 @@ class Fractal
 
     private static function lastId($data)
     {
-        if ($data instanceof \Illuminate\Support\Collection) return $data->last()->id;
+        if ($data instanceof \Illuminate\Support\Collection) return $data->last()->id ?? null;
         if (is_array($data)) {
             $last = end($data);
             if (isset($last['id'])) return $last['id'];
